19b15c1d59079ff7a521c41b049d8a66445ad9a5
phanhoang510 <nguyenquangminh@tasconnect.com>
2023-07-05 01:14:42 +0700

    23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PinComponent } from './pin/pin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PinComponent
   ],
   imports: [
     BrowserModule
